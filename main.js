22cefb4d45d0bb4e7dd41fc42c3fde0bbb9e2f69
macktsawyer <macktsawyer@yahoo.com>
2021-04-25 14:48:31 -0400

    Links started, pop up cards started

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 'use strict';
 
 // Pop up cards
+
+let cardButtonOne = document.getElementById('cardButtonOne');
+let cardButtonTwo = document.getElementById('cardButtonTwo');
+let cardButtonThree = document.getElementById('cardButtonThree');
+
