ef480daba25872c9c8543a576983be1d4ac8e053
macktsawyer <macktsawyer@yahoo.com>
2021-05-04 19:40:23 -0400

    Added popup functionality

--- main.js
+++ main.js
@@ -2,6 +2,35 @@
 
 // Pop up cards
 
-let cardButtonOne = document.getElementById('cardButtonOne');
-let cardButtonTwo = document.getElementById('cardButtonTwo');
-let cardButtonThree = document.getElementById('cardButtonThree');
+// Close pop-up by clicking outside of box
+// Make everything look nicer
+
+document.getElementById('cardButtonOne').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('#projectOnePopup').style.display = 'flex';
+});
+
+document.getElementById('exitOneX').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+    document.querySelector('#projectOnePopup').style.display = 'none';
+});
+
+document.getElementById('cardButtonTwo').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('#projectTwoPopup').style.display = 'flex';
+});
+
+document.getElementById('exitTwoX').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+    document.querySelector('#projectTwoPopup').style.display = 'none';
+});
+
+document.getElementById('cardButtonThree').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('#projectThreePopup').style.display = 'flex';
+});
+
+document.getElementById('exitThreeX').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+    document.querySelector('#projectThreePopup').style.display = 'none';
+});
